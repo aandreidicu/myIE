82fdf7bd8d3ed9e005e1997b6099c01aac25dc45
Andrei Dicu <andrei.dicu01@gmail.com>
2023-12-15 00:15:31 +0200

    feat: Add Custom User Model

--- myIE/urls.py
+++ myIE/urls.py
@@ -21,3 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('supersecret/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header= "MyIE Admin"
+admin.site.site_title= "MyIE Admin Portal"
+admin.site.index_title= "Welcome to MyIE Portal"
