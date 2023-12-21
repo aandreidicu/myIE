56469d68eb916b6ddd0b35067e7363a57ea567b6
Andrei Dicu <andrei.dicu01@gmail.com>
2023-12-21 09:41:06 +0200

    feat: Update user profile

--- myIE/settings/development.py
+++ myIE/settings/development.py
@@ -6,9 +6,9 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = env("EMAIL_PORT")
 EMAIL_HOST_USER = env("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = "info_myIE@myie.com"
+DEFAULT_FROM_EMAIL = "info_exin@ei.com"
 DOMAIN = env("DOMAIN")
-SITE_NAME = "MyIE"
+SITE_NAME = "EXIN"
 
 DATABASES = {
     "default": {
--- myIE/urls.py
+++ myIE/urls.py
@@ -7,8 +7,9 @@ urlpatterns = [
     path("supersecret/", admin.site.urls),
     path("api/v1/auth/", include("djoser.urls")),
     path("api/v1/auth/", include("djoser.urls.jwt")),
+    path("api/v1/profile/", include("apps.profiles.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-admin.site.site_header= "MyIE Admin"
-admin.site.site_title= "MyIE Admin Portal"
-admin.site.index_title= "Welcome to MyIE Portal"
+admin.site.site_header= "EXIN Admin"
+admin.site.site_title= "EXIN Admin Portal"
+admin.site.index_title= "Welcome to EXIN Portal"
